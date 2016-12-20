9f91f89a188bf439ac2e8b5cc4f92ef40524f5bc
John Bottenberg <jobotten@microsoft.com>
2016-12-19 17:35:06 -0800

    Add templates for generating TryParseUri overloads, plus tests

---
+++ tests/System.Text.Primitives.Tests/PrimitiveParserUriPerfTests.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Globalization;
+using Xunit;
+using Microsoft.Xunit.Performance;
+using System.Text.Internal;
+
+namespace System.Text.Primitives.Tests
+{
+    public partial class PrimitiveParserPerfTests
+    {
+        [Benchmark]
+        [InlineData("https://my.uri.djkadjald.org/wiki/This_is_a_Universal#Resource_Identifier")]
+        private static void BaselineStringToUri(string text)
+        {
+            Uri uri;
+            foreach (var iteration in Benchmark.Iterations)
+            {
+                using (iteration.StartMeasurement())
+                {
+                    for (int i = 0; i < LoadIterations; i++)
+                    {
+                        Uri.TryCreate(text, UriKind.Absolute, out uri);
+                    }
+                }
+            }
+        }
+
+        [Benchmark]
+        [InlineData("https://my.uri.djkadjald.org/wiki/This_is_a_Universal#Resource_Identifier")]
+        private static void InternalParserByteSpanToUri(string text)
+        {
+            Uri uri;
+            int bytesConsumed;
+            byte[] utf8ByteArray = Encoding.UTF8.GetBytes(text);
+            ReadOnlySpan<byte> utf8ByteSpan = new ReadOnlySpan<byte>(utf8ByteArray);
+            foreach (var iteration in Benchmark.Iterations)
+            {
+                using (iteration.StartMeasurement())
+                {
+                    for (int i = 0; i < LoadIterations; i++)
+                    {
+                        InternalParser.TryParseUri(utf8ByteSpan, out uri, out bytesConsumed);
+                    }
+                }
+            }
+        }
+
+        [Benchmark]
+        [InlineData("https://my.uri.djkadjald.org/wiki/This_is_a_Universal#Resource_Identifier")]
+        private unsafe static void InternalParserByteStarToUri(string text)
+        {
+            Uri uri;
+            int bytesConsumed;
+            byte[] utf8ByteArray = Encoding.UTF8.GetBytes(text);
+            foreach (var iteration in Benchmark.Iterations)
+            {
+                fixed (byte* utf8ByteStar = utf8ByteArray)
+                {
+                    using (iteration.StartMeasurement())
+                    {
+                        for (int i = 0; i < LoadIterations; i++)
+                        {
+                            InternalParser.TryParseUri(utf8ByteStar, 0, utf8ByteArray.Length, out uri, out bytesConsumed);
+                        }
+                    }
+                }
+            }
+        }
+
+        [Benchmark]
+        [InlineData("https://my.uri.djkadjald.org/wiki/This_is_a_Universal#Resource_Identifier")]
+        private static void PrimitiveParserByteSpanToUri(string text)
+        {
+            Uri uri;
+            int bytesConsumed;
+            byte[] utf8ByteArray = Encoding.UTF8.GetBytes(text);
+            ReadOnlySpan<byte> utf8ByteSpan = new ReadOnlySpan<byte>(utf8ByteArray);
+            foreach (var iteration in Benchmark.Iterations)
+            {
+                using (iteration.StartMeasurement())
+                {
+                    for (int i = 0; i < LoadIterations; i++)
+                    {
+                        PrimitiveParser.InvariantUtf8.TryParseUri(utf8ByteSpan, out uri, out bytesConsumed);
+                    }
+                }
+            }
+        }
+
+        [Benchmark]
+        [InlineData("https://my.uri.djkadjald.org/wiki/This_is_a_Universal#Resource_Identifier")]
+        private unsafe static void PrimitiveParserByteStarToUri(string text)
+        {
+            Uri uri;
+            int bytesConsumed;
+            byte[] utf8ByteArray = Encoding.UTF8.GetBytes(text);
+            foreach (var iteration in Benchmark.Iterations)
+            {
+                fixed (byte* utf8ByteStar = utf8ByteArray)
+                {
+                    using (iteration.StartMeasurement())
+                    {
+                        for (int i = 0; i < LoadIterations; i++)
+                        {
+                            PrimitiveParser.InvariantUtf8.TryParseUri(utf8ByteStar, utf8ByteArray.Length, out uri, out bytesConsumed);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ tests/System.Text.Primitives.Tests/PrimitiveParserUriTests.cs
@@ -0,0 +1,65 @@
+﻿using Xunit;
+
+namespace System.Text.Primitives.Tests
+{
+    public partial class PrimitiveParserTests
+    {
+        [Theory]
+        // TODO: Add test cases with invalid Uri strings
+        // default(Uri) is null, so parsedValue.ToString() will throw NullReferenceException
+        [InlineData("https://my.uri.djkadjald.org/wiki/This_is_a_Universal#Resource_Identifier", true, "https://my.uri.djkadjald.org/wiki/This_is_a_Universal#Resource_Identifier", 73)]
+        public unsafe void ParseUriInvariant(string text, bool expectSuccess, string expectedValue, int expectedConsumed)
+        {
+            Uri parsedValue;
+            int consumed;
+            ReadOnlySpan<byte> utf8Span = UtfEncode(text, false);
+            ReadOnlySpan<char> utf16Span = new ReadOnlySpan<byte>(UtfEncode(text, true)).Cast<byte, char>();
+            byte[] textBytes = utf8Span.ToArray();
+            char[] textChars = utf16Span.ToArray();
+            bool result;
+
+            result = PrimitiveParser.InvariantUtf8.TryParseUri(utf8Span, out parsedValue);
+            Assert.Equal(expectSuccess, result);
+            Assert.Equal(expectedValue, parsedValue.ToString());
+
+            result = PrimitiveParser.InvariantUtf8.TryParseUri(utf8Span, out parsedValue, out consumed);
+            Assert.Equal(expectSuccess, result);
+            Assert.Equal(expectedValue, parsedValue.ToString());
+            Assert.Equal(expectedConsumed, consumed);
+
+            fixed (byte* arrayPointer = textBytes)
+            {
+                result = PrimitiveParser.InvariantUtf8.TryParseUri(arrayPointer, textBytes.Length, out parsedValue);
+
+                Assert.Equal(expectSuccess, result);
+                Assert.Equal(expectedValue, parsedValue.ToString());
+
+                result = PrimitiveParser.InvariantUtf8.TryParseUri(arrayPointer, textBytes.Length, out parsedValue, out consumed);
+                Assert.Equal(expectSuccess, result);
+                Assert.Equal(expectedValue, parsedValue.ToString());
+                Assert.Equal(expectedConsumed, consumed);
+            }
+
+            result = PrimitiveParser.InvariantUtf16.TryParseUri(utf16Span, out parsedValue);
+            Assert.Equal(expectSuccess, result);
+            Assert.Equal(expectedValue, parsedValue.ToString());
+
+            result = PrimitiveParser.InvariantUtf16.TryParseUri(utf16Span, out parsedValue, out consumed);
+            Assert.Equal(expectSuccess, result);
+            Assert.Equal(expectedValue, parsedValue.ToString());
+            Assert.Equal(expectedConsumed, consumed);
+
+            fixed (char* arrayPointer = textChars)
+            {
+                result = PrimitiveParser.InvariantUtf16.TryParseUri(arrayPointer, textBytes.Length, out parsedValue);
+                Assert.Equal(expectSuccess, result);
+                Assert.Equal(expectedValue, parsedValue.ToString());
+
+                result = PrimitiveParser.InvariantUtf16.TryParseUri(arrayPointer, textBytes.Length, out parsedValue, out consumed);
+                Assert.Equal(expectSuccess, result);
+                Assert.Equal(expectedValue, parsedValue.ToString());
+                Assert.Equal(expectedConsumed, consumed);
+            }
+        }
+    }
+}
